91c3c1ff5faafd577ed08d540090e6ef8402b73b
Saket Narayan <saket@saket.me>
2024-11-27 02:18:35 -0500

    Add a paparazzi test to verify drag interaction

--- library/src/test/kotlin/me/saket/squiggles/SquigglySliderTest.kt
+++ library/src/test/kotlin/me/saket/squiggles/SquigglySliderTest.kt
@@ -3,6 +3,8 @@
 package me.saket.squiggles
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,17 +13,24 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode
+import kotlinx.coroutines.delay
 import org.junit.Rule
 import org.junit.Test
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalMaterial3Api::class)
 class SquigglySliderTest {
@@ -29,6 +38,33 @@ class SquigglySliderTest {
     deviceConfig = DeviceConfig.PIXEL_5,
     renderingMode = RenderingMode.SHRINK,
   )
+
+  @Test fun `flatten squiggles when slider is dragged`() {
+    paparazzi.gif(2.seconds) {
+      Scaffold {
+        val interactionSource = remember { MutableInteractionSource() }
+        SquigglySlider(
+          value = 0.5f,
+          onValueChange = {},
+          interactionSource = interactionSource,
+          squigglesSpec = SquigglySlider.SquigglesSpec(
+            wavelength = 48.dp,
+            amplitude = 4.dp,
+            strokeWidth = 8.dp,
+          )
+        )
+
+        // Mimic a drag gesture on the slider.
+        LaunchedEffect(Unit) {
+          delay(0.5.seconds)
+          val start = DragInteraction.Start()
+          interactionSource.emit(start)
+          delay(1.seconds)
+          interactionSource.emit(DragInteraction.Stop(start))
+        }
+      }
+    }
+  }
 
   @Test fun `zero slider value`() {
     paparazzi.snapshot {
@@ -119,3 +155,16 @@ private fun <T> stateOf(value: T): State<T> {
   return mutableStateOf(value)
 }
 
+private fun Paparazzi.gif(
+  duration: Duration = 500.milliseconds,
+  fps: Int = 60,
+  content: @Composable () -> Unit,
+) {
+  val view = ComposeView(context)
+  view.setContent(content)
+  gif(
+    view = view,
+    end = duration.inWholeMilliseconds,
+    fps = fps,
+  )
+}
